78e2b2833f82df193717f5e849c671659f013094
langkammer <robsonlangkammer@hotmail.com>
2016-02-09 22:19:25 -0200

    melhorias front grunt

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -286,32 +286,44 @@ module.exports = function (grunt) {
         }
       }
     },
+    imagemin: {
+      dist: {
+        files: [
+          {
+            expand: true,
+            cwd: '<%= yeoman.app %>/images',
+            src: '**/*.{png,jpg,jpeg,gif}',
+            dest: '<%= yeoman.dist %>/images'
+          }
+        ]
+      }
+    },
 
-    // The following *-min tasks will produce minified files in the dist folder
-    // By default, your `index.html`'s <!-- Usemin block --> will take care of
-    // minification. These next options are pre-configured if you do not wish
-    // to use the Usemin blocks.
-    // cssmin: {
-    //   dist: {
-    //     files: {
-    //       '<%= yeoman.dist %>/styles/main.css': [
-    //         '.tmp/styles/{,*/}*.css'
-    //       ]
-    //     }
-    //   }
-    // },
-    // uglify: {
-    //   dist: {
-    //     files: {
-    //       '<%= yeoman.dist %>/scripts/scripts.js': [
-    //         '<%= yeoman.dist %>/scripts/scripts.js'
-    //       ]
-    //     }
-    //   }
-    // },
-    // concat: {
-    //   dist: {}
-    // },
+     //The following *-min tasks will produce minified files in the dist folder
+     //By default, your `index.html`'s <!-- Usemin block --> will take care of
+     //minification. These next options are pre-configured if you do not wish
+     //to use the Usemin blocks.
+     cssmin: {
+       dist: {
+         files: {
+           '<%= yeoman.dist %>/styles/main.css': [
+             '.tmp/styles/{,*/}*.css'
+           ]
+         }
+       }
+     },
+     uglify: {
+       dist: {
+         files: {
+           '<%= yeoman.dist %>/scripts/scripts.js': [
+             '<%= yeoman.dist %>/scripts/scripts.js'
+           ]
+         }
+       }
+     },
+     concat: {
+       dist: {}
+     },
 
     svgmin: {
       dist: {
@@ -530,8 +542,10 @@ module.exports = function (grunt) {
     'cdnify',
     'cssmin',
     'uglify',
-    'filerev'
+    'filerev',
+    'imagemin'
   ]);
+
 
 
   grunt.registerTask('build-play', [
